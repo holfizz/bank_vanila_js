eac5fc549846c15a3e0aa43f42535389e7b10cbf
holfizz <gorlach7v@gmial.com>
2023-09-19 21:05:05 +0300

    add render service, seo.config, layout component and base screen component

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,5 +1,15 @@
-export class Home {
-	render() {
-		return '<p>Home</p>'
+import { BaseScreen } from '@/core/component/base-screen.component'
+import template from './home.template.html'
+import RenderService from '@/core/services/render.service'
+import styles from './home.module.scss'
+
+export class Home extends BaseScreen {
+	constructor () {
+		super({ title: 'Home' })
+	}
+
+	render () {
+		const element = RenderService.htmlToElement(template, [], styles)
+		return element.outerHTML
 	}
 }
